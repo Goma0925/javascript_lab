5ccf0abd1ab90672a7558eb2e7794a64261bd4f0
Newcomb Maddux <newcomb@Newcomb.local>
2019-02-20 17:39:05 -0500

    first draft

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 var makeList = function()
 {
-
+  console.log("it worked");
   var list = document.createElement("ul");
   var li = document.createElement('li');
   li.innerText = "Lion";
   list.appendChild(li);
-
+  console.log(list);
+  console.log(li);
 }
-var button = document.getElementById('createList')
+makeList();
+var button = document.getElementById("createList");
+console.log(button);
+button.onclick = function(){makeList()};
