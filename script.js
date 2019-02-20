fa5e59faf92a0dbea69a56d652c7cdd4693e57da
Newcomb Maddux <newcomb@Newcomb.local>
2019-02-20 17:26:42 -0500

    first

---
+++ script.js
@@ -0,0 +1,10 @@
+var makeList = function()
+{
+
+  var list = document.createElement("ul");
+  var li = document.createElement('li');
+  li.innerText = "Lion";
+  list.appendChild(li);
+
+}
+var button = document.getElementById('createList')
