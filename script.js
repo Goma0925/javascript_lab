7dd4ba952f6223bd167ff3e3c28d14c5d266d5b8
Newcomb Maddux <newcomb@Newcomb.local>
2019-02-20 18:08:56 -0500

    success

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
-var makeList = function()
-{
-  console.log("it worked");
-  var list = document.createElement("ul");
-  var li = document.createElement('li');
-  li.innerText = "Lion";
-  list.appendChild(li);
-  console.log(list);
-  console.log(li);
-}
-makeList();
+
+
 var button = document.getElementById("createList");
 console.log(button);
-button.onclick = function(){makeList()};
+
+var makeList = function()
+{
+  var button = document.getElementById("createList").removeAttribute("onclick");
+  var content = document.getElementById("content");
+  var list = document.createElement("ul");
+  var animalList = ["Lion", 'Ostrich', "Zebra","Gazelle"];
+  console.log('Content;' + content);
+  animalList.forEach(function(d){
+    var li = document.createElement('li');
+    li.innerText = d;
+    content.appendChild(list);
+    list.appendChild(li);
+
+  });
+}
