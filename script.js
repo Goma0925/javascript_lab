4767256f3a66bb8a3d3cfb4b8045d6c238bc4823
Goma0925 <amonbali0925@gmail.com>
2019-02-20 18:39:00 -0500

    Completed

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ var makeList = function()
   var button = document.getElementById("createList").removeAttribute("onclick");
   var content = document.getElementById("content");
   var list = document.createElement("ul");
+  list.setAttribute("id", "list");
   var animalList = ["Lion", 'Ostrich', "Zebra","Gazelle"];
   console.log('Content;' + content);
   animalList.forEach(function(d){
@@ -16,4 +17,17 @@ var makeList = function()
     content.appendChild(list);
     list.appendChild(li);
   });
+
+  var addForm = document.getElementById("add-form");
+  addForm.style.visibility = "visible";
+}
+
+var addButton = document.getElementById("add");
+var addAnimal = function(){
+var list = document.getElementById("list");
+console.log(list);
+var animalForm = document.getElementById("text-box");
+var li = document.createElement('li');
+li.innerText = animalForm.value;
+list.appendChild(li);
 }
